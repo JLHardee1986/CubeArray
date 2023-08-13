f4d48128e98b8c7fdd70b84684106b1e46ed3503
aCIDman <jhardee@augusta.edu>
2023-08-13 01:39:22 -0400

    MyGui TextBox working with MyCube , using myCube as a button which prompt a text box for a password input or something

--- Cube.h
+++ Cube.h
@@ -2,15 +2,20 @@
 #define __CUBE__
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Textbox.h"
 
-struct Cube : public sf::Drawable, sf::Transformable
+class  Cube : public sf::Drawable, sf::Transformable
 {
-private:
+public:
 	sf::VertexArray m_vertices;
 	sf::VertexArray m_points;
 	sf::VertexArray m_lines;
 	sf::Color m_color;
 	sf::Vector2f m_pos;
+	bool m_isTransparent{false};
+	sf::Font aFont;
+	TextBox* makeTxt() { return new TextBox(22, sf::Color(sf::Color(255, 255, 255, 255)), true, aFont, { 100.f, 100.f }); }
+	TextBox* currTextBox{ nullptr };
 public:
 	Cube(sf::Color color, sf::Vector2f pos, sf::Vector2u size) 
 		: m_color(color)
@@ -81,20 +86,20 @@ public:
 
 
 		
-		m_vertices[4].position = { m_pos.x + size.x - (size.x / 3.f)  + 1.f, m_pos.y + (2.f * (size.y / 3.f)) + -2.5f};
-		m_vertices[5].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2)  -1.5f,  m_pos.y + (size.y / 3.f) -0.5f };
-		m_vertices[6].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2)  - 2.f,  m_pos.y + (size.y / 3.f) + j  -1.f };
-		m_vertices[7].position = { m_pos.x + size.x - (size.x / 3.f) + 1.0f , m_pos.y + (2.f * (size.y / 3.f)) - 5.f + j };
+		m_vertices[4].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f))};
+		m_vertices[5].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2),  m_pos.y + (size.y / 3.f) };
+		m_vertices[6].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2),  m_pos.y + (size.y / 3.f) + j};
+		m_vertices[7].position = { m_pos.x + size.x - (size.x / 3.f) , m_pos.y + (2.f * (size.y / 3.f)) + j };
 	
-		m_vertices[8].position = { m_pos.x -1.f, m_pos.y + (size.y / 3.f) + j -0.f };
-		m_vertices[9].position = { m_pos.x - 1.f, m_pos.y + (size.y / 3.f) -1.f};
-		m_vertices[10].position = { m_pos.x + size.x - (size.x / 3.f) +1.f, m_pos.y + (2.f * (size.y / 3.f)) -1.f  };
-		m_vertices[11].position = { m_pos.x + size.x - (size.x / 3.f) +1.5f , m_pos.y + (2.f * (size.y / 3.f)) +1.f + j };
+		m_vertices[8].position = { m_pos.x, m_pos.y + (size.y / 3.f) + j };
+		m_vertices[9].position = { m_pos.x , m_pos.y + (size.y / 3.f) };
+		m_vertices[10].position = { m_pos.x + size.x - (size.x / 3.f) , m_pos.y + (2.f * (size.y / 3.f))  };
+		m_vertices[11].position = { m_pos.x + size.x - (size.x / 3.f)  , m_pos.y + (2.f * (size.y / 3.f)) + j };
 
-		m_vertices[0].position = { m_pos.x +2.f, m_pos.y + (size.y / 3.f) + 0.f };
-		m_vertices[1].position = { m_pos.x + size.x * (2 / 3.f), m_pos.y -0.5f };
-		m_vertices[2].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2) -1, m_pos.y + (size.y / 3.f) -0.5f };
-		m_vertices[3].position = { m_pos.x + size.x - (size.x / 3.f) -1.f, m_pos.y + (2.f * (size.y / 3.f)) -1.f };
+		m_vertices[0].position = { m_pos.x , m_pos.y + (size.y / 3.f) };
+		m_vertices[1].position = { m_pos.x + size.x * (2 / 3.f), m_pos.y };
+		m_vertices[2].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2) , m_pos.y + (size.y / 3.f)  };
+		m_vertices[3].position = { m_pos.x + size.x - (size.x / 3.f) , m_pos.y + (2.f * (size.y / 3.f))  };
 
 		
 
@@ -144,17 +149,17 @@ public:
 		std::cout << accumulator << std::endl;
 
 
-		m_lines[0].position = { m_pos.x - 2.f, m_pos.y + (size.y / 3.f) - 1.f };
-		m_lines[1].position = { m_pos.x + size.x - (size.x / 3.f) - 1, m_pos.y + (2.f * (size.y / 3.f)) - 0.5f };
+		m_lines[0].position = { m_pos.x, m_pos.y + (size.y / 3.f)};
+		m_lines[1].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f)) };
 
-		m_lines[2].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f)) - 1 };
-		m_lines[3].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2) - 1, m_pos.y + (size.y / 3.f) - 1 };
+		m_lines[2].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f))  };
+		m_lines[3].position = { m_pos.x + ((size.x - (size.x / 3.f)) * 2) , m_pos.y + (size.y / 3.f)  };
 
-		m_lines[4].position = { m_pos.x + size.x - (size.x / 3.f)+0, m_pos.y + (2.f * (size.y / 3.f)) - 1 };
-		m_lines[5].position = { m_pos.x + size.x - (size.x / 3.f)+0, m_pos.y + (2.f * (size.y / 3.f)) + j + 1};
+		m_lines[4].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f)) };
+		m_lines[5].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f)) + j };
 
-		m_lines[6].position = { m_pos.x + size.x - (size.x / 3.f)-1, m_pos.y + (2.f * (size.y / 3.f)) - 1 };
-		m_lines[7].position = { m_pos.x + size.x - (size.x / 3.f)-1, m_pos.y + (2.f * (size.y / 3.f)) + j + 1 };
+		m_lines[6].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f))  };
+		m_lines[7].position = { m_pos.x + size.x - (size.x / 3.f), m_pos.y + (2.f * (size.y / 3.f)) + j  };
 
 
 		m_lines[0].color = sf::Color::Black;
@@ -168,13 +173,42 @@ public:
 
 		m_lines[6].color = sf::Color::Black;
 		m_lines[7].color = sf::Color::Black;
+
+		aFont.loadFromFile("fonts/Roboto.ttf");
+
+	
+		
 	}
 
+	void move(const sf::Vector2f& moveAmt)
+	{
+		m_pos.x += moveAmt.x;
+		m_pos.y += moveAmt.y;
 
+		this->move(m_pos);
+
+		
+	}
+	void recolor()
+	{
+		m_vertices[0].color = m_color;
+		m_vertices[1].color = m_color;
+		m_vertices[2].color = m_color;
+		m_vertices[3].color = m_color;
+		m_vertices[4].color = m_color;
+		m_vertices[5].color = m_color;
+		m_vertices[6].color = m_color;
+		m_vertices[7].color = m_color;
+		m_vertices[8].color = m_color;
+		m_vertices[9].color = m_color;
+		m_vertices[10].color = m_color;
+		m_vertices[11].color = m_color;
+
+		return;
+	}
+	
 	void draw(sf::RenderTarget& window, sf::RenderStates states) const override
 	{
-		
-		
 	
 
 		window.draw(m_points);
@@ -184,6 +218,68 @@ public:
 		window.draw(m_lines);
 
 
+	}
+
+	void lookThrough(bool isTransparent = false)
+	{
+		if (isTransparent)
+		{
+			m_color.a = 50;
+		}
+		else
+		{
+			m_color.a = 180;
+		}
+		recolor();
+	}
+
+	void Toggle(sf::Color newColor = sf::Color::Transparent)
+	{
+		this->m_color = newColor;
+		this->recolor();
+	}
+
+
+	void update(float dt, const sf::RenderWindow& window)
+	{
+		sf::Vector2f m_mousePos = (sf::Vector2f)sf::Mouse::getPosition(window);
+		sf::FloatRect hitBox({ m_pos.x, m_pos.y, 128, 64 });
+		if (hitBox.contains(m_mousePos))
+		{
+			//  User is hovering over the cube, ditch all its panels alpha values;
+			
+			lookThrough(true);
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
+			{
+			
+				Toggle();
+
+			}
+
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+			{
+
+				Toggle(sf::Color(this->m_color.r + 1 % 255, this->m_color.g + 1 % 255, this->m_color.b + 1 % 255, this->m_color.a + 1 % 255));
+				
+				if (!currTextBox)
+					currTextBox = makeTxt();
+
+				
+
+			}
+
+			
+		}
+		else
+		{
+			lookThrough(false);
+		}
+		
+	}
+	~Cube()
+	{
+		if (currTextBox)
+			delete currTextBox;
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -12,10 +12,15 @@ int main()
 	settings.antialiasingLevel = 8;
 	std::unique_ptr<sf::RenderWindow> upWnd = std::make_unique<sf::RenderWindow>(sf::VideoMode(640U, 480U, 32U), "Template", sf::Style::Fullscreen, settings);
 
-	Cube aCube(sf::Color(0,155,0,255), { 100.f, 100.f }, { 64U , 64U });
-
+	Cube aCube(sf::Color(155,155,255,255), { 100.f, 100.f }, { 64U , 64U });
+	float dt;
+	TextBox* tBox = nullptr;
+	sf::Clock frameTime;
 	while (upWnd->isOpen())
 	{
+
+
+		dt = frameTime.restart().asSeconds();
 		sf::Event e;
 		while (upWnd->pollEvent(e))
 		{
@@ -29,16 +34,37 @@ int main()
 					{
 						upWnd->close();
 					}
+					else if (e.key.code == sf::Keyboard::T)
+					{
+
+					}
+					break;
+				case sf::Event::TextEntered:
+					if (tBox)
+						tBox->typedOn(e);
 					break;
 				default:
 					break;
 			}
 
 		}
+
+		aCube.update(dt, *upWnd);
+		if (aCube.currTextBox)
+		{
+			tBox = aCube.currTextBox;
+		}
+
 		upWnd->clear(sf::Color(sf::Color::Black));
 		upWnd->draw(aCube);
+		if (tBox)
+		{
+			tBox->drawTo(*upWnd);
+		}
 		upWnd->display();
 	}
 
 	return EXIT_SUCCESS;
 }
+
+
